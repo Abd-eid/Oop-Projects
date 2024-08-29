af691a8b56767348eed8e830938fd46f733d7a50
Abd-eid <abdalrazzaqeid@gmail.com>
2024-08-30 01:06:34 +0300

    Add files via upload

---
+++ Shapes/src/main/java/com/mycompany/shapes/Shapes.java
@@ -0,0 +1,23 @@
+
+
+package com.mycompany.shapes;
+
+
+public class Shapes {
+
+   public void draw(){
+        System.out.println("Drawing a shape");
+    }
+    
+    public static void main(String[] args) {
+        Circle c1=new Circle();
+        Rectangle r1 = new Rectangle();
+        Shapes sh1= new Shapes();
+        
+        c1.draw();
+        r1.draw();
+        r1.draw(8);
+        sh1.draw();
+        
+    }
+}
