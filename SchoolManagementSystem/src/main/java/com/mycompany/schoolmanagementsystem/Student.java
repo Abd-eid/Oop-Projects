4dc063b971eed24f448a0b017d9fa5ec01219e6e
Abd-eid <abdalrazzaqeid@gmail.com>
2024-09-21 13:58:11 +0300

    Add files via upload

---
+++ SchoolManagementSystem/src/main/java/com/mycompany/schoolmanagementsystem/Student.java
@@ -0,0 +1,40 @@
+
+package com.mycompany.schoolmanagementsystem;
+
+
+public class Student extends Person {
+
+    private String grade;
+    private String studentID;
+
+    public Student(String name, int age, String address, String grade, String studentID) {
+        super(name, age, address);
+        this.grade = grade;
+        this.studentID = studentID;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+    
+    public void study(){
+         System.out.println(getName() + " is studying in grade " + grade);
+    }
+    
+    String role(){
+        return "Student";
+    }
+    
+}
